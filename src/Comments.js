e551a6c3e019be2e216bf69bc672c590444c5df9
Rankrish <59313760+purusr@users.noreply.github.com>
2022-12-13 18:33:32 -0800

    Update Comments.js

--- src/Comments.js
+++ src/Comments.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import imgg from './pexels.jpg'
 
 function Comments (){
   const {id} = useParams()
@@ -11,28 +10,34 @@ function Comments (){
 
   useEffect(()=>{
     axios.get(`http://localhost:5000/getComment/${id}`).then(res => setCmntimage(res.data))
-  }, [1])
+  }, [comment,id])
 
-console.log(cmntimage);
+console.log(comment);
   const updateComment = (id) =>{
     axios.post(`http://localhost:5000/updateComments/${id}`, {comment: comment})
   }
     return(
          <div className="container text-center">
-            <div className="row">
-            <div class="col col-sm-3">
-              <img src={imgg} alt="logo" style={{ width: '700px', height: '450px', padding: '20px' }}/>
-              <br/>
+            <div className="col">
+              <div class=" row">
+                <img src={cmntimage && cmntimage.image_url} alt="logo" style={{ width: '700px', height: '450px', padding: '20px' }}/>
+                <br/>
+              </div>
+              <hr/>
             </div>
-            <hr/>
-            <div className='col'>
-                <input onChange={(e) => setComment(e.target.value)} type='text' name='comment' value={comment}></input>
-                <button onClick={()=>{updateComment()}}>Comment</button>
+            <div className='row'>
+              <div className='col'>
+                  <input className='form-control' onChange={(e) => setComment(e.target.value)} type='text' name='comment' value={comment}></input>
+                  <br/>
+                  <button style={{ float: 'left' }} onClick={()=>{updateComment(id)}}>Comment</button>
+              </div>
             </div>
-            <div className='col text-start'>
-              <p>This is a sample comment to display here.</p>
+            <br/>
+            {cmntimage && cmntimage.comments.map( (cmntdata) =>
+            <div className='row text-start'>
+                <p> {cmntdata}</p>
             </div>
-            </div>
+            )}
         </div>
     )
 }
