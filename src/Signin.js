e22c5e1e1b9df42350978f7cce78f6ff44b4cf04
Rankrish <59313760+purusr@users.noreply.github.com>
2022-12-20 18:44:12 -0800

    update

--- src/Signin.js
+++ src/Signin.js
@@ -1,17 +1,33 @@
+import { useState } from "react"
+import axios from "axios"
+
 const Signin = ()=>{
+
+const [email, setEmail] = useState()
+const [loginpassword, setLoginPassword] = useState()
+const handleSubmitLogin= (e)=>{
+    e.preventDefault()
+    axios.post('http://localhost:5000/user/signin', {email: email, password: loginpassword})
+    .then((res) =>{
+        console.log(res)
+    }).catch((error) =>{
+        console.log('An error occured')
+    })
+
+}
     return(
         <div className="container text-center">
         <div style={{ padding: '50px' }}>
         <form>
             <div className="text-center"  style={{ width: '400px', display: 'inline-block', padding:'5px' }}>
-            <input className="form-control" placeholder="E-mail"></input>
+            <input onChange={(e) =>{setEmail(e.target.value)}} value={email} className="form-control" placeholder="E-mail"></input>
             </div>
             <br/>
             <div className="text-center"  style={{ width: '400px', display: 'inline-block', padding:'5px'}}>
-            <input className="form-control" placeholder="Password"></input>
+            <input  onChange={(e) =>{setLoginPassword(e.target.value)}} value={loginpassword} className="form-control" placeholder="Password"></input>
             </div>
             <br/>
-            <button className="btn btn-primary pa-10" type="submit">Sign In</button>
+            <button onClick={(e) => handleSubmitLogin(e)} className="btn btn-primary pa-10" type="submit">Sign In</button>
         </form>
         </div>
         </div>
