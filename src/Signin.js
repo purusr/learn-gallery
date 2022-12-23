1758d73a7b914872ba85e57b686f90b503d617e0
Rankrish <59313760+purusr@users.noreply.github.com>
2022-12-22 18:35:42 -0800

    updated

--- src/Signin.js
+++ src/Signin.js
@@ -1,22 +1,38 @@
 import { useState } from "react"
 import axios from "axios"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
 
 const Signin = ()=>{
 
+const navigate = useNavigate();
 const [email, setEmail] = useState()
 const [loginpassword, setLoginPassword] = useState()
+const user = localStorage.getItem('galleryprofile')
+
+
+if(user){
+navigate('/')
+}
+
 const handleSubmitLogin= (e)=>{
     e.preventDefault()
     axios.post('http://localhost:5000/user/signin', {email: email, password: loginpassword})
     .then((res) =>{
-        console.log(res)
+        toast('Login Success')
+        localStorage.setItem('galleryProfile', JSON.stringify(res.data))
+        navigate('/')
+
     }).catch((error) =>{
         console.log('An error occured')
     })
-
+    setEmail('')
+    setLoginPassword('')
 }
     return(
         <div className="container text-center">
+            <ToastContainer/>
         <div style={{ padding: '50px' }}>
         <form>
             <div className="text-center"  style={{ width: '400px', display: 'inline-block', padding:'5px' }}>
@@ -26,7 +42,11 @@ const handleSubmitLogin= (e)=>{
             <div className="text-center"  style={{ width: '400px', display: 'inline-block', padding:'5px'}}>
             <input  onChange={(e) =>{setLoginPassword(e.target.value)}} value={loginpassword} className="form-control" placeholder="Password"></input>
             </div>
-            <br/>
+            <div>
+            <a href='/signup' style={{padding:'5px'}}>New here? click to Sign Up</a>
+            </div>
+            
+            <br/> 
             <button onClick={(e) => handleSubmitLogin(e)} className="btn btn-primary pa-10" type="submit">Sign In</button>
         </form>
         </div>
